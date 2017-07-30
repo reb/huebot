af0a7ffd562ea25954af904f36eb0c64f25d0009
reb <reboumans@gmail.com>
2017-07-30 13:25:18 +0200

    Move Slack interactions to a module

--- huebot.py
+++ huebot.py
@@ -3,92 +3,16 @@ from time import sleep
 from traceback import print_exc
 from configparser import ConfigParser
 from configparser import Error as ConfigParserError
-from slackclient import SlackClient
 import huebot.state
 import huebot.hue
-
-
-slack_client = None
-slack_channels = {}
-
-# These are the colors for slack attachment messages
-GOOD = '36a64f'
-WARNING = 'daa038'
-DANGER = 'd00000'
-
-
-def get_channel_name(channel):
-    """Retrieve the a channel's name from the Slack API."""
-    global slack_channels
-
-    try:
-        return slack_channels[channel]
-    except KeyError:
-        try:
-            response = slack_client.api_call('channels.info', channel=channel)
-            name = response['channel']['name']
-            slack_channels[channel] = name
-            return name
-
-        except KeyError:
-            response = slack_client.api_call('groups.info', channel=channel)
-            name = response['group']['name']
-            slack_channels[channel] = name
-            return name
-
-
-def parse_test_message(output, state):
-    """Parse user written test messages."""
-    try:
-        channel = get_channel_name(output['channel'])
-        text = output['text']
-    except KeyError:
-        return False
-
-    if text == 'failure':
-        state.failure(channel)
-    elif text == 'warning':
-        state.warning(channel)
-    elif text == 'normal':
-        state.normal(channel)
-
-
-def parse_jenkins_message(output, state):
-    """Parse Jenkins written messages."""
-    try:
-        channel = get_channel_name(output['channel'])
-        color = output['attachments'][0]['color']
-    except KeyError:
-        return False
-
-    if "-test-" in channel:
-        if color == GOOD:
-            state.normal(channel)
-        elif color == WARNING:
-            state.warning(channel)
-        elif color == DANGER:
-            state.warning(channel)
-    else:
-        if color == GOOD:
-            state.normal(channel)
-        elif color == WARNING:
-            state.warning(channel)
-        elif color == DANGER:
-            state.failure(channel)
-
-
-def parse_slack_output(rtm_output, state):
-    """Parse the Slack RTM API events firehose for relevant messages."""
-    for output in rtm_output:
-        parse_test_message(output, state)
-        parse_jenkins_message(output, state)
+import huebot.slack
 
 
 if __name__ == '__main__':
     config = ConfigParser()
     try:
         config.read('huebot.ini')
-        slack_bot_token = config['SLACK']['Token']
+        slack_token = config['SLACK']['Token']
         hue_bridge_ip = config['HUE']['BridgeIp']
         debug = config.getboolean('GENERAL', 'Debug', fallback=False)
 
@@ -101,15 +25,12 @@ if __name__ == '__main__':
     while True:
         try:
             huebot.hue.HueLights(hue_bridge_ip, state)
+            slack = huebot.slack.Slack(slack_token, state)
 
-            slack_client = SlackClient(slack_bot_token)
-            if slack_client.rtm_connect():
-                print("HueBot connected to Slack!")
-                while True:
-                    parse_slack_output(slack_client.rtm_read(), state)
-                    sleep(1)
-            else:
-                print("HueBot did not connect, check bot ID and Slack token")
+            while(True):
+                slack.read()
+                sleep(1)
+
         except KeyboardInterrupt:
             exit()
         except:
---
+++ huebot/slack.py
@@ -0,0 +1,114 @@
+"""Slack interactions module for HueBot."""
+from slackclient import SlackClient
+
+# These are the colors for slack message attachments
+GOOD = '36a64f'
+WARNING = 'daa038'
+DANGER = 'd00000'
+
+
+def _parse(func):
+    """
+    Indicate that the function parses the Slack RTM API output.
+
+    Expected is that it takes one line of the RTM output and handles
+    any potential state changes using the self.state object.
+    """
+    func._parse_func = True
+    return func
+
+
+class Slack:
+    """Object to connect to Slack and react to messages."""
+
+    def __init__(self, slack_token, state):
+        """Connect to Slack and setup the object."""
+        self.channels = {}
+        self.client = SlackClient(slack_token)
+        if self.client.rtm_connect():
+            print("HueBot connected to Slack!")
+        else:
+            print("HueBot did not connect to Slack")
+            raise IOError
+
+        self.state = state
+
+        # Retrieve parse functions
+        self.parse_functions = []
+        for attr_name in dir(self):
+            attr = getattr(self, attr_name)
+            try:
+                if attr._parse_func:
+                    self.parse_functions.append(attr)
+            except AttributeError:
+                pass
+
+    def get_channel_name(self, channel):
+        """Get the channel name for the given channel."""
+        try:
+            return self.channels[channel]
+        except KeyError:
+            try:
+                return self.__retrieve_channel_name(channel)
+
+            except KeyError:
+                return self.__retrieve_group_name(channel)
+
+    def __retrieve_channel_name(self, channel):
+        response = self.client.api_call('channels.info', channel=channel)
+        name = response['channel']['name']
+        self.channels[channel] = name
+        return name
+
+    def __retrieve_group_name(self, group):
+        response = self.client.api_call('groups.info', channel=group)
+        name = response['group']['name']
+        self.channels[group] = name
+        return name
+
+    def read(self):
+        """Read the output from the RTM stream."""
+        rtm_output = self.client.rtm_read()
+        for output in rtm_output:
+            for parse in self.parse_functions:
+                parse(output)
+
+    @_parse
+    def __test(self, output):
+        """Parse user written test messages."""
+        try:
+            channel = self.get_channel_name(output['channel'])
+            text = output['text']
+        except KeyError:
+            return False
+
+        if text == 'failure':
+            self.state.failure(channel)
+        elif text == 'warning':
+            self.state.warning(channel)
+        elif text == 'normal':
+            self.state.normal(channel)
+
+    @_parse
+    def __jenkins(self, output):
+        """Parse Jenkins written messages."""
+        try:
+            channel = self.get_channel_name(output['channel'])
+            color = output['attachments'][0]['color']
+        except KeyError:
+            return False
+
+        if "-test-" in channel:
+            if color == GOOD:
+                self.state.normal(channel)
+            elif color == WARNING:
+                self.state.warning(channel)
+            elif color == DANGER:
+                self.state.warning(channel)
+        else:
+            if color == GOOD:
+                self.state.normal(channel)
+            elif color == WARNING:
+                self.state.warning(channel)
+            elif color == DANGER:
+                self.state.failure(channel)
